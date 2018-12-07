30328debd82abfe87c9ffb63ba69934bbf5c6efb
glen <glen_sale@yahoo.com>
2018-12-07 15:25:50 -0800

    it will add emplyee/new Form by using the controller

--- HairSalon/Controllers/EmployeeController.cs
+++ HairSalon/Controllers/EmployeeController.cs
@@ -11,17 +11,32 @@ namespace HairSalon.Controllers
     [HttpGet("/employee")]
     public ActionResult Index()
     {
-      return View();
+      List<Employee> allemployee = Employee.GetAll();
+         return View(allemployee);
     }
 
-    [HttpPost("/showemployee")]
-        public ActionResult Create(string Name)
-        {
-            Employee newEmployee = new Employee(Name);
-            List<Employee> allemployee = Employee.GetAll();
-            return View("Index", allemployee);
-
-        }
+    [HttpGet("/employee/new")]
+    public ActionResult New()
+  {
+      return View();
+  }
+  [HttpPost("/employee")]
+     public ActionResult Create(string Name)
+     {
+         Employee newEmployee = new Employee(Name);
+         List<Employee> allemployee = Employee.GetAll();
+         return View("Index", allemployee);
+     }
+     [HttpGet("/employee/{id}")]
+          public ActionResult Show(int Id)
+          {
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              Employee newEmployee = Employee.Find(Id);
+              List<Employee> allemployee = newEmployee.GetName();
+              model.Add("employee", newEmployee);
+              model.Add("details", details);
+              return View(model);
+          }
 
   }
 }
