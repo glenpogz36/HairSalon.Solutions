42cdd70cbaa619ab41622a9081aac44f6a3b12e9
glen <glen_sale@yahoo.com>
2018-12-09 22:09:34 -0800

    it will try to execute testing

---
+++ HairSalon.Tests/ModelTests/ClienTests.cs
@@ -0,0 +1,34 @@
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using System;
+// using System.Collections.Generic;
+// using HairSalon.Models;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace HairSalon.Test
+// {
+//   [TestClass]
+//     public class ClientTests : IDisposable
+//     {
+//         public void Dispose()
+//         {
+//             Client.DeleteAll();
+//         }
+//         public ClientTests()
+//         {
+//             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=mark_mangahas_tests;";
+//         }
+//
+//
+//
+//       [TestMethod]
+//       public void GetAll_DbStartsEmpty_0()
+//       {
+//         //Arrange
+//         //Act
+//         int result = Client.GetAll().Count;
+//
+//       //Assert
+//       Assert.AreEqual(0, result);
+//       }
+//     }
+//   }
