606fe6569efc7a68344bb99dcffb78ad8df32cf1
glen <glen_sale@yahoo.com>
2018-12-09 17:41:39 -0800

    it will add save function

--- HairSalon/Models/Employee.cs
+++ HairSalon/Models/Employee.cs
@@ -30,12 +30,12 @@ namespace HairSalon.Models
       MySqlConnection conn = DB.Connection();
       conn.Open();
       MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"SELECT * from hairsalon;";
+      cmd.CommandText = @"SELECT * from employee;";
       MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
       while (rdr.Read())
       {
-        int Id = rdr.GetInt32(0);
-        string Name = rdr.GetString(1);
+        int Id = rdr.GetInt32(1);
+        string Name = rdr.GetString(0);
         Employee newEmployee = new Employee(Name, Id);
         allemployee.Add(newEmployee);
       }
@@ -51,7 +51,7 @@ namespace HairSalon.Models
       MySqlConnection conn = DB.Connection();
       conn.Open();
       var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"SELECT * FROM hairsalon WHERE id = (@searchId);";
+      cmd.CommandText = @"SELECT * FROM employee WHERE id = (@searchId);";
       MySqlParameter searchId = new MySqlParameter();
       searchId.ParameterName = "@searchId";
       searchId.Value = id;
@@ -61,7 +61,7 @@ namespace HairSalon.Models
       string Name = "";
       while (rdr.Read())
       {
-        Id = rdr.GetInt32(0);
+        Id = rdr.GetInt32(1);
         Name = rdr.GetString(0);
       }
       Employee newEmployee = new Employee(Name, Id);
@@ -72,5 +72,45 @@ namespace HairSalon.Models
       }
       return newEmployee;
     }
+
+    public override bool Equals(System.Object newEmployee)
+  {
+    if (!(newEmployee is Employee))
+    {
+      return false;
+    }
+    else
+    {
+      Employee newEmployee = (Employee) newEmployee;
+      bool idEquality = (this.GetId() == newEmployee.GetId());
+      bool nameEquality = (this.GetName() == newEmployee.GetName());
+      return (idEquality && nameEquality);
+    }
   }
-}
+
+    public void Save()
+      {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+
+        var cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"INSERT INTO employees (name) VALUES (@EmployeeName);";
+
+        MySqlParameter name = new MySqlParameter();
+        name.ParameterName = "@EmployeeName";
+        name.Value = this._name;
+        cmd.Parameters.Add(name);
+
+        cmd.ExecuteNonQuery();
+        _id = (int) cmd.LastInsertedId;
+        //
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+      }
+
+
+    }
+  }
