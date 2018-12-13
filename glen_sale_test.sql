5b2f1c3ea785c1d447729b2dc2b8e8e3127d9459
Guest User <Guest@epicodus-118.local>
2018-12-13 15:00:16 -0800

    it will redo databse

--- glen_sale_test.sql
+++ glen_sale_test.sql
@@ -1,26 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 12, 2018 at 07:28 AM
--- Server version: 5.6.34-log
--- PHP Version: 7.2.1
+-- Generation Time: Dec 13, 2018 at 11:59 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `glen_sale`
+-- Database: `glen_sale_test`
 --
+CREATE DATABASE IF NOT EXISTS `glen_sale_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `glen_sale_test`;
 
 -- --------------------------------------------------------
 
@@ -94,8 +88,4 @@ ALTER TABLE `clients`
 -- AUTO_INCREMENT for table `stylists`
 --
 ALTER TABLE `stylists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
